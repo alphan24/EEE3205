89c92f84a2fbd11935085333a12c0738d405ff0e
mehmet can alphan <mehmetcan.alphan@gmail.com>
2019-11-19 01:17:25 +0300

    Update write_EEE.c

--- MikroC_examples/Seven_segment/write_EEE.c
+++ MikroC_examples/Seven_segment/write_EEE.c
@@ -1,1 +1,19 @@
+void main() 
+{
+PORTA = 0;//clear PORTA   
+PORTD = 0;//clear PORTD  
+TRISD = 0;//configure PORTA  as output
+TRISA = 0;//configure PORTD  as output
+
+    	
+    	PORTD = 0x79;// Letter E; turn on a-d-e-f-g segments PORTD = 0b01111001
+      while(1){//Create an infinite loop
+        PORTA.B3 = 1;// enable the 1st digit from left (DIS3) by setting 3rd bit of PORTA register
+        delay_ms(10);//Give 10ms delay
+        PORTA.B2 = 1;// enable the 2nd digit from left (DIS2) by setting 2nd bit of PORTA register
+        delay_ms(10);//Give 10ms delay
+        PORTA.B1 = 1;// enable the 3rd digit from left (DIS1) by setting 1st bit of PORTA register
+        delay_ms(10);//Give 10ms delay
+      }
+}
 
